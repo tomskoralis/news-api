66830d5b8de4b043a1cd7efd9735bf4eab99d9e5
tomskoralis <tomskoralis@gmail.com>
2022-11-28 11:34:32 +0200

    Updated Router

--- app/Router.php
+++ app/Router.php
@@ -2,26 +2,28 @@
 
 namespace App;
 
-use FastRoute\Dispatcher;
-use FastRoute\RouteCollector;
+use FastRoute\{Dispatcher, RouteCollector};
 use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
+use Twig\Error\{LoaderError, RuntimeError, SyntaxError};
+use Twig\Loader\FilesystemLoader;
 use function FastRoute\simpleDispatcher;
 
 class Router
 {
     private Dispatcher $dispatcher;
+    private Environment $twig;
+    private ApiAccess $newsApi;
 
     public function __construct()
     {
+        $this->newsApi = new ApiAccess();
+        $this->twig = new Environment(new FilesystemLoader('views'));
         $this->dispatcher = simpleDispatcher(function (RouteCollector $route) {
             $route->addRoute('GET', '/', ['App\Controllers\SearchResultsController', 'index']);
         });
     }
 
-    public function handleUri(Environment $twig, ApiAccess $newsApi): void
+    public function handleUri(): void
     {
         $httpMethod = $_SERVER['REQUEST_METHOD'];
         $uri = $_SERVER['REQUEST_URI'];
@@ -34,7 +36,7 @@ class Router
         switch ($routeInfo[0]) {
             case Dispatcher::NOT_FOUND:
                 try {
-                    echo $twig->render('templates/404.twig');
+                    echo $this->twig->render('templates/404.twig');
                 } catch (LoaderError $e) {
                     echo "Twig Loader Error: " . $e->getMessage();
                 } catch (RuntimeError $e) {
@@ -46,7 +48,7 @@ class Router
             case Dispatcher::METHOD_NOT_ALLOWED:
 //                $allowedMethods = $routeInfo[1];
                 try {
-                    echo $twig->render('templates/405.twig');
+                    echo $this->twig->render('templates/405.twig');
                 } catch (LoaderError $e) {
                     echo "Twig Loader Error: " . $e->getMessage();
                 } catch (RuntimeError $e) {
@@ -59,7 +61,7 @@ class Router
                 $handler = $routeInfo[1];
 //                $vars = $routeInfo[2];
                 [$controller, $method] = $handler;
-                echo (new $controller)->$method($twig, $newsApi);
+                echo (new $controller)->$method($this->twig, $this->newsApi);
                 break;
         }
     }
--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-use App\{ApiAccess, Router};
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
+use App\Router;
 
 require_once 'vendor/autoload.php';
 require_once 'app/constants.php';
 
-$newsApi = new ApiAccess();
-$twig = new Environment(new FilesystemLoader('views'));
 $router = new Router();
-$router->handleUri($twig, $newsApi);
+$router->handleUri();
